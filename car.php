c8109ec554512696e0bfad02034be60d22ca7d90
Andrew Devlin and Brendan Grubb <brendan@>
2017-02-06 14:08:27 -0800

    add mileage to form and logic and display message if no conditions are met

--- car.php
+++ car.php
@@ -1,17 +1,19 @@
-<?php
+`<?php
 class Car
 {
     private $make_model;
     private $price;
     private $miles;
     private $style;
+    private $picture;
 
-    function __construct($make_model, $price, $miles, $style)
+    function __construct($make_model, $price, $miles, $style, $picture)
     {
         $this->make_model = $make_model;
         $this->price = $price;
         $this->miles = $miles;
         $this->style = $style;
+        $this->picture = $picture;
     }
 
     function setMakeModel($new_make_model)
@@ -38,6 +40,11 @@ class Car
         return $new_miles;
     }
 
+    function setPicture($new_picture)
+    {
+        $this->picture = $new_picture;
+    }
+
     function getMakeModel()
     {
         return $this->make_model;
@@ -54,23 +61,29 @@ class Car
     {
         return $this->style;
     }
-
-    function worthBuying($max_price)
+    function getPicture()
     {
-        return $this->price < ($max_price + 100);
+        return $this->picture;
     }
+
+    function worthBuying($max_price, $max_mileage)
+    {
+        if($max_price >= $this->price && $max_mileage >= $this->miles)
+        return $this->price && $this->miles;
+    }
+
 }
 
-$porsche = new Car("2014 Porsche 911", 114991, 7864, "Sedan");
-$ford = new Car("2011 Ford F450", 55995, 14241, "Coupe");
-$lexus = new Car("2013 Lexus RX 350", 44700, 20000, "Sedan");
-$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "Coupe");
+$porsche = new Car("2014 Porsche 911", 114991, 7864, "Sedan", "img/porsche.jpg");
+$ford = new Car("2011 Ford F450", 55995, 14241, "Coupe", "img/ford.jpg");
+$lexus = new Car("2013 Lexus RX 350", 44700, 20000, "Sedan", "img/lexus.jpg");
+$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "Coupe", "img/benz.jpg");
 
 $cars = array($porsche, $ford, $lexus, $mercedes);
 
 $cars_matching_search = array();
 foreach ($cars as $car) {
-    if ($car->worthBuying($_GET["price"])) {
+    if ($car->worthBuying($_GET["price"], $_GET["mileage"])) {
         array_push($cars_matching_search, $car);
     }
 }
@@ -80,19 +93,28 @@ foreach ($cars as $car) {
 <html>
 <head>
     <title>Your Car Dealership's Homepage</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
     <h1>Your Car Dealership</h1>
     <ul>
         <?php
-            foreach ($cars_matching_search as $car) {
-                $price = $car->getPrice();
-                $name = $car->getMakeModel();
-                $total_mileage = $car->getMiles();
-                $car_style = $car->getStyle();
-                echo "<ul>
-                        <li><em>name:</em> <strong>$name</strong>($car_style) - <em>price:</em> <strong>$price</strong> - <em>mileage:</em> <strong>$total_mileage</strong></li>
-                    </ul>";
+            if (empty($cars_matching_search)) {
+                echo "<li>There are no cars matching your search</li>";
+            }
+            else {
+                foreach ($cars_matching_search as $car) {
+                    $price = $car->getPrice();
+                    $name = $car->getMakeModel();
+                    $total_mileage = $car->getMiles();
+                    $car_style = $car->getStyle();
+                    $car_picture = $car->getPicture();
+                    echo "<img src='$car_picture' alt= 'picture of $name'/>
+                        <ul>
+                            <li><em>name:</em> <strong>$name</strong>($car_style) - <em>price:</em> <strong>$price</strong> - <em>mileage:</em> <strong>$total_mileage</strong></li>
+                        </ul>";
+                }
             }
 
             // $new_name = $ford->setMakeModel("Ugly Ass Car");
